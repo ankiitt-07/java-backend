7841361136025a4714208c61bfff77e97f985c91
ankiitt-07 <ak077636@gmail.com>
2025-04-22 14:31:10 +0530

    [Ankit] Add: Rest UC5: added Put request

---
+++ 02 - Spring_Intro/restapi/src/main/java/com/restapi/controllers/RestApiController.java
@@ -0,0 +1,32 @@
+package com.restapi.controllers;
+
+import com.restapi.dtos.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RestApiController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+    @GetMapping("/query")
+    public String query(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/param/{name}")
+    public String param(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @PostMapping("/post")
+    public String post(@RequestBody UserDto userDTO) {
+        return "Hello " + userDTO.getFirstName() + " " + userDTO.getLastName();
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String put(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName;
+    }
+}
