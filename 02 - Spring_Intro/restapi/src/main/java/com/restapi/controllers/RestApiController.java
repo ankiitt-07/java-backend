7be90ddff9b2333d0cb3c7c63e1470848c82d849
ankiitt-07 <ak077636@gmail.com>
2025-04-22 14:37:03 +0530

    [Ankit] Add: Rest UC1: added rest controller

--- 02 - Spring_Intro/restapi/src/main/java/com/restapi/controllers/RestApiController.java
+++ 02 - Spring_Intro/restapi/src/main/java/com/restapi/controllers/RestApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestApiController {
+    
     @GetMapping("/")
     public String index() {
         return "Hello World";
