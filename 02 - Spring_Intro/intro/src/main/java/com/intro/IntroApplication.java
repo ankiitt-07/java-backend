d287db899cc98382bfc9736b871094097cd91d03
ankiitt-07 <ak077636@gmail.com>
2025-04-22 11:58:19 +0530

    [Ankit] Add: Spring P2 UC 3 : demonstrate dependency injection using EmployeeBean and DepartmentBean

--- 02 - Spring_Intro/intro/src/main/java/com/intro/IntroApplication.java
+++ 02 - Spring_Intro/intro/src/main/java/com/intro/IntroApplication.java
@@ -1,13 +1,21 @@
 package com.intro;
 
+import com.intro.beans.Employee;
+import com.intro.controllers.HelloRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class IntroApplication {
+    private static final Logger log = LoggerFactory.getLogger(IntroApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(IntroApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(IntroApplication.class, args);
+        log.info("Checking Context: {}", context.getBean(Employee.class));
+
     }
 
 }
