3431c39877194d1aa8cfb36a3f3bc82a51e23710
ankiitt-07 <ak077636@gmail.com>
2025-04-23 11:38:22 +0530

    [Ankit] Add: Address book UC2: Added rest controllers

--- 03 -  Spring Rest/address-book/src/main/java/com/addressbook/controllers/AddressBookController.java
+++ 03 -  Spring Rest/address-book/src/main/java/com/addressbook/controllers/AddressBookController.java
@@ -1,9 +1,8 @@
 package com.addressbook.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/address-book")
@@ -11,5 +10,25 @@ public class AddressBookController {
     @GetMapping()
     public ResponseEntity<String> index(){
         return ResponseEntity.ok("Hello World from Spring AddressBook");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> get(@PathVariable int id){
+        return ResponseEntity.ok("Hello World from Spring AddressBook at ID: " + id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> post(){
+        return ResponseEntity.ok("Hello World from Spring AddressBook at POST");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> put(@PathVariable int id){
+        return ResponseEntity.ok("Hello World from Spring AddressBook at PUT at ID: " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id){
+        return ResponseEntity.ok("Hello World from Spring AddressBook at DELETE at ID: " + id);
     }
 }
